be983a05b7e4f620a9f1cae495c17898fb66e403
Paulo Augusto <augustoempresariall@gmail.com>
2025-11-22 00:59:05 -0300

    Projeto Nikel, iniciando o GIT

---
+++ public/JS/index.js
@@ -0,0 +1,103 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkedLogged();
+
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const session = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if (!account) {
+        alert("Verifique o usuário ou a senha.");
+        return;
+    }
+
+    if (account) {
+        if (account.password !== password) {
+            alert("Verifique o usuário ou a senha.");
+            return;
+        }
+
+        saveSession(email, session);
+
+        window.location.href = "home.html";
+
+    }
+});
+
+
+//CRIAR CONTA
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if (email.length < 5) {
+        alert("Por favor, insira um email válido.");
+        return;
+    }
+
+    if (password.length < 8) {
+        alert("A senha deve ter pelo menos 8 caracteres.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+
+})
+
+function checkedLogged() {
+    if (session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if (logged) {
+        saveSession(logged, session);
+        
+        window.location.href = "home.html";
+    }
+
+}
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+
+function saveSession(data, saveSession) {
+    if (saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if (account) {
+        return JSON.parse(account);
+    }
+
+    return "";
+}
